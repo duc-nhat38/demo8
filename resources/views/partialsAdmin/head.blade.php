fd3fab280e85a41711f0c8fbf971b80084f71a41
duc-nhat38 <phamdinhduc02071998@gmail.com>
2020-08-27 09:57:39 +0700

    fix js

--- resources/views/partialsAdmin/head.blade.php
+++ resources/views/partialsAdmin/head.blade.php
@@ -9,13 +9,13 @@
     <title id="title-name">@yield('title', 'Dashboard')</title>
 
   <!-- Custom fonts for this template-->
-  <link href=" {{asset('vendor/fontawesome-free/css/all.min.css')}} " rel="stylesheet" type="text/css">
+  <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="{{asset('css/sb-admin-2.css')}}" rel="stylesheet">
+  <link href="/css/sb-admin-2.css" rel="stylesheet">
 
-  <link rel="stylesheet" href="{{ asset('css/dashboard-css.css') }}">
+  <link rel="stylesheet" href="/css/dashboard-css.css">
   {{-- datatable --}}
   <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
 
