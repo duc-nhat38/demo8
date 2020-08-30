e979ef656b25c98a82e117115f73cfdec26058d9
duc-nhat38 <phamdinhduc02071998@gmail.com>
2020-08-30 20:17:17 +0700

    fix csrf

--- public/js/homePage/Banner.js
+++ public/js/homePage/Banner.js
@@ -30,5 +30,10 @@ banner.getSlideBanner = function () {
     });
 }
 $(document).ready(function () {
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
     banner.getSlideBanner();
 });
